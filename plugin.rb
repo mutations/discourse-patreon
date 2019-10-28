722c17380f2fe4f2e33fc73fdb563054b6fb5760
Danny Bosan <dan@mutations.ltd>
2019-10-28 13:02:08 -0700

    fix logging error

--- plugin.rb
+++ plugin.rb
@@ -147,16 +147,18 @@ after_initialize do
         response_parsed = response.parsed
         Rails.logger.info("response_parsed.keys: #{response_parsed.keys}")
         Rails.logger.info("response_parsed.keys: #{response_parsed.to_yaml}")
+        Rails.logger.info("response_parsed[:links]: #{response_parsed[:links].to_yaml}")
+        Rails.logger.info("response_parsed[:links][:self]: #{response_parsed[:links][:self].to_yaml}")
 
         self_response = begin
-          client.request(:get, response.parsed[:links][:self], headers: {
+          client.request(:get, response_parsed[:links][:self], headers: {
               'Authorization' => "Bearer #{access_token.token}"
           }, parse: :json).parsed
         rescue => e
           Rails.logger.warn("Error while getting campaign info with error: #{e}.\n\n #{e.backtrace.join("\n")}")
           {}
         end
-        Rails.logger.info("self_response.parsed: #{self_response.parsed.to_yaml}")
+        Rails.logger.info("self_response: #{self_response.inspect}")
 
         campaign_response = begin
           client.request(:get, "https://api.patreon.com/oauth2/api/current_user/campaigns", headers: {
